9ca5842ac81eaa2550c3bead998a496a9239e6b4
ricardocossta <cossta.ricardo@gmail.com>
2024-09-16 02:26:35 -0300

    feat: implement RabbitMQ consumer and MongoDB integration
    - Added RabbitMQ consumer to process messages from the queue
    - Implemented message handling with deserialization of cryptocurrency data
    - Integrated MongoDB repository for storing and updating cryptocurrency prices
    - Added error handling and logging for message processing

---
+++ CryptoCoinTracker/CryptoCoinTracker.DataProcessor/Models/Crypto.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+
+namespace CryptoCoinTracker.DataProcessor.Models;
+
+public class Crypto
+{
+    [JsonProperty("bitcoin")]
+    public CryptoData? Bitcoin { get; set; }
+
+    [JsonProperty("solana")]
+    public CryptoData? Solana { get; set; }
+
+    [JsonProperty("ethereum")]
+    public CryptoData? Ethereum { get; set; }
+
+    [JsonProperty("binancecoin")]
+    public CryptoData? BinanceCoin { get; set; }
+
+    [JsonProperty("dogecoin")]
+    public CryptoData? DogeCoin { get; set; }
+}
+
+public class CryptoData
+{
+    [JsonProperty("usd")]
+    public decimal Usd { get; set; }
+
+    [JsonProperty("usd_market_cap")]
+    public decimal UsdMarketCap { get; set; }
+}
