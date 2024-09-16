ec88195f4947553ed652fa64e4aab0aa1b10e546
ricardocossta <cossta.ricardo@gmail.com>
2024-09-15 22:03:23 -0300

    chore: Add dependency injection of RabbitMq Client

--- CryptoCoinTracker/CryptoCoinTracker.DataFetcher/Program.cs
+++ CryptoCoinTracker/CryptoCoinTracker.DataFetcher/Program.cs
@@ -1,7 +1,9 @@
 using CryptoCoinTracker.DataFetcher;
 using CryptoCoinTracker.DataFetcher.Interfaces;
+using CryptoCoinTracker.DataFetcher.MessageBus;
 using CryptoCoinTracker.DataFetcher.Models.Settings;
 using CryptoCoinTracker.DataFetcher.Service;
+using RabbitMQ.Client;
 
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddHostedService<Worker>();
@@ -11,6 +13,13 @@ builder.Services.AddSingleton<IDataCollectorService, DataCollectorService>();
 builder.Services.AddHttpClient();
 
 builder.Services.Configure<CoinGeckoApiSettings>(builder.Configuration.GetSection("CoinGeckoApi"));
+
+//RabbitMq
+var connectionFactory = new ConnectionFactory { HostName = "localhost", };
+var connection = connectionFactory.CreateConnection();
+builder.Services.AddSingleton(new ProducerConnection(connection));
+
+builder.Services.AddSingleton<IMessageBusClient, RabbitMqClient>();
 
 var host = builder.Build();
 host.Run();
